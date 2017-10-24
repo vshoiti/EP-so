6013eb7c1be38aa7b9f685b6c422500b83b081e3
Gabriel Arruda <gabrielpardi@outlook.com>
2017-10-23 22:13:03 -0200

    Criei uma classe separada para Runnable.

--- src/EP.java
+++ src/EP.java
@@ -43,7 +43,6 @@ public class EP {
         //RODANDO AS THREADS
         for (int i = 0; i < 100; i++) {
             threadsArray[i].start();
-
             try{
                 threadsArray[i].sleep(1);
             }catch(InterruptedException e){}
@@ -54,43 +53,5 @@ public class EP {
             System.out.println(lines[i]);
         }
 
-    }
-}
-
-class Reader implements Runnable{
-
-    String[] lines;
-    /**
-     * No PDF: "Se o objeto for um leitor, ele deve somente ler a palavra na posição desejada,
-     * armazenando-a em alguma variável local..."
-     * A variável local pode ser dentro do Runnable??? (String read)
-     */
-    String read;
-
-    Reader(String[] lines){
-        this.lines = lines;
-    }
-
-    public void run(){
-        for (int i = 0; i < 100; i++) {
-            int index = (int) (Math.random() * 36242);
-            this.read = this.lines[index];
-        }
-    }
-}
-
-class Writer implements Runnable{
-
-    String[] lines;
-
-    Writer(String[] lines){
-        this.lines = lines;
-    }
-
-    public void run(){
-        for (int i = 0; i < 100; i++) {
-            int index = (int) (Math.random()*36242);
-            this.lines[index] = "MODIFICADO";
-        }
     }
 }
---
+++ src/MyRunnable.java
@@ -0,0 +1,44 @@
+public abstract class MyRunnable implements Runnable{
+
+    String[] lines;
+
+    publoc void run();
+}
+
+class Reader extends MyRunnable{
+
+    String[] lines;
+    /**
+     * No PDF: "Se o objeto for um leitor, ele deve somente ler a palavra na posição desejada,
+     * armazenando-a em alguma variável local..."
+     * A variável local pode ser dentro do Runnable??? (String read)
+     */
+    String read;
+
+    Reader(String[] lines){
+        this.lines = lines;
+    }
+
+    public void run(){
+        for (int i = 0; i < 100; i++) {
+            int index = (int) (Math.random() * 36242);
+            this.read = this.lines[index];
+        }
+    }
+}
+    
+class Writer extends MyRunnable{
+
+    String[] lines;
+
+    Writer(String[] lines){
+        this.lines = lines;
+    }
+
+    public void run(){
+        for (int i = 0; i < 100; i++) {
+            int index = (int) (Math.random()*36242);
+            this.lines[index] = "MODIFICADO";
+        }
+    }
+}
